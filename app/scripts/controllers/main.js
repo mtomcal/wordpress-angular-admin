07ee2ba28ce5ffe6d791c776d71867c1fd070ee9
Michael A Tomcal <michael@twoeyetech.com>
2013-08-18 17:46:26 -0700

    Added loader animation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,10 +3,13 @@
 angular.module('wpamin')
   .controller('MainCtrl', function ($scope, Process) {
     $scope.message = "";
+    $scope.loader = false;
 
     $scope.toggle = function () {
+      $scope.loader = true;
       Process.query(function (data) {
         $scope.message = data["Message"];
+        $scope.loader = false;
       });
     };
 
--- views/settings.php
+++ views/settings.php
@@ -2,6 +2,7 @@
   <div ng-controller="MainCtrl" ng-cloak>
   <h2>Admin Settings</h2>
   <button class="button-primary" ng-click="toggle()">Test JSON Call</button>
+  <img ng-show="loader" src="/wp-admin/images/wpspin_light.gif" alt="" />
   <p>{{message}}</p>
   </div>
 </div>
